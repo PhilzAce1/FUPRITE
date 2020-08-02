ce2793ea78be75e600993513c7f6522385914f24
Philzace1 <unknownumeh@gmail.com>
2020-08-03 00:19:30 +0100

    so

--- server/util/db.js
+++ server/util/db.js
@@ -4,8 +4,8 @@ const mongoose = require('mongoose');
 module.exports = () => {
   mongoose
     .connect(
-      'mongodb+srv://philzace:0sSiByLfbRuXW2Kv@cluster0-ojdiw.mongodb.net/test?retryWrites=true&w=majority',
-
+      'mongodb+srv://ace:philzpy162@cluster0.cl6wg.mongodb.net/Fuprepeeps?retryWrites=true&w=majority',
+      // 'mongodb+srv://philzace:0sSiByLfbRuXW2Kv@cluster0-ojdiw.mongodb.net/test?retryWrites=true&w=majority',
       // 'mongodb://localhost/Fblog',
       // process.env.MONGO_URI ||
       //  ||
